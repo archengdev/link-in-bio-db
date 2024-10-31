da9417d2fed6791aca6a63d049cacf4656ba7d64
First Draft <grades@firstdraft.com>
2024-10-31 20:42:21 +0000

    Update spec/ folder to latest version

--- spec/models/01_link_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-describe "Link" do
-  it "has a class defined in app/models/", points: 1 do
-    expect{ Link }.to_not raise_error,
-      "Expected a Link class to be defined in the app/models/ folder, but didn't find one."
-  end
-end
